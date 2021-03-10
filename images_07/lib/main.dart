2d508be95ebb70b0682d2a8209c164c6fc6fb837
KurbanAli6615 <kurabanalimasu@gmail.com>
2021-03-10 11:23:31 +0530

    Flutter

--- images_07/lib/main.dart
+++ images_07/lib/main.dart
@@ -7,6 +7,33 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: "Icons",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Icons Widget"),
+          backgroundColor: Colors.black,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(28.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+               Column(
+                children: [
+                  Image.asset("assets/a.jpg",width: 300,),
+                  Text("Wēãpøn X",style: TextStyle(
+                    fontSize: 30
+                  ),
+                  ),
+                  Spacer(),
+                  Image.network("https://media.giphy.com/media/YWUpVw86AtIbe/giphy.gif",width: 200,)
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
